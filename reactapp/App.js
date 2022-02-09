934bd77977813e6ab73049854ae7df2685aea2a9
jhansisabbavarapu123 <96778739+jhansisabbavarapu123@users.noreply.github.com>
2022-02-09 10:58:26 +0530

    Add files via upload

--- reactapp/App.js
+++ reactapp/App.js
@@ -1,59 +1,31 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import './App.css';
-class App extends Component {
-  constructor(props){  // constructor will support for one time load of the class
-   super(props);    // initialize Component class properties here
-   this.state = {address:'what is your address',doornumber:"153 0-132",street:"Kondapalem",city:"Garividi",pincode:"535101",District:"Vizianagaram"};  
-   }  
-   render() {  
-           return (  
-               <div>  
-                   welcome to {this.props.name}<br/>
-                   <TestMe/>
-                   {this.state.address}<br/>
-                   <h2>Address:</h2>
-                   <AddressSender doornumber={this.state.doornumber} street={this.state.street} city={this.state.city} pincode={this.state.pincode} District={this.state.District}></AddressSender>
-               <table>
-                 <tr>
-                   <td>DoorNo</td>
-                   <td>{this.state.doornumber}</td>
-                </tr>
-                <tr>
-                  <td>street</td>
-                  <td>{this.state.street}</td>
-                </tr>
-                <tr>
-                  <td>City</td>
-                  <td>{this.state.city}</td>
-                </tr>
-                <tr>
-                  <td>District</td>
-                  <td>{this.state.District}</td>
-                  </tr>
-                <tr>
-                  <td>Pincode</td>
-                  <td>{this.state.pincode}</td>
-                </tr>
-               </table>
-               </div>);  
- }
- }
- App.defaultProps={
-     name:"Jhansi"
- }
- class TestMe extends Component{
-     render(){
-         return(<div><font face='Calibre' color='blue' size='20' >Hello All</font></div>);
-     }
- }
- class AddressSender extends Component{
-     render(){
-         return(<div><font color='red'>
-           {this.props.doornumber}
-           {this.props.street}
-           {this.props.city}
-           {this.props.District}
-           {this.props.pincode}</font></div>);
-     }
- }
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={value:''};
+    this.handleChange=this.handleChange.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this); 
+  }
+  handleChange(event){
+    this.setState({value:event.target.value});
+  }
+  handleSubmit(event){
+    alert('you have submitted the input successfully:' +this.state.value);
+    event.preventDefault();
+    document.getElementById("sp").innerHTML= '<br/> you have submitted the input successfully: '+this.state.value;
+  }
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <h1>Controlled Form Example</h1>
+        <label>Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        </label>
+        <input type="submit" value="Submit"/>
+        <span id="sp"></span>
+      </form>
+    );
+  }
+}
 export default App;
