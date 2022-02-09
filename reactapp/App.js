334562fc9303c38a87c0c83c805164212e96e35d
jhansisabbavarapu123 <96778739+jhansisabbavarapu123@users.noreply.github.com>
2022-02-09 10:55:50 +0530

    Add files via upload

--- reactapp/App.js
+++ reactapp/App.js
@@ -1,39 +1,59 @@
 import React,{Component} from 'react';
 import './App.css';
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state={ message:prompt("Enter message","my message"),mobile:'1234567890'}
-   
-  }  
-  render(){
-    return(<div>
-      Welcome to {this.props.name}<br/>
-      Available in {this.props.location}<br/>
-      Training in{this.props.job}
-      <TestMe/>
-     <br/>
-      <MessageSender message={this.state.message} mobile={this.state.mobile}></MessageSender>
-    </div> );
-  }
-}
-App.defaultProps = {  
-  name: "Jhansi" ,
-  location:"India",
-  job:"software"
-}  
-class TestMe extends Component{
-  render(){
-    return(<font face='calibre' color="pink" size='20'>Welcome to Application development</font>);
-  }
-}
-class MessageSender extends Component{
-  render(){
-    return(<div>
-  
-      <hr/>
-      <font color='blue'>{this.props.message}</font>
-           <h3>{this.props.mobile}</h3></div>);
-  }
-}
+class App extends Component {
+  constructor(props){  // constructor will support for one time load of the class
+   super(props);    // initialize Component class properties here
+   this.state = {address:'what is your address',doornumber:"153 0-132",street:"Kondapalem",city:"Garividi",pincode:"535101",District:"Vizianagaram"};  
+   }  
+   render() {  
+           return (  
+               <div>  
+                   welcome to {this.props.name}<br/>
+                   <TestMe/>
+                   {this.state.address}<br/>
+                   <h2>Address:</h2>
+                   <AddressSender doornumber={this.state.doornumber} street={this.state.street} city={this.state.city} pincode={this.state.pincode} District={this.state.District}></AddressSender>
+               <table>
+                 <tr>
+                   <td>DoorNo</td>
+                   <td>{this.state.doornumber}</td>
+                </tr>
+                <tr>
+                  <td>street</td>
+                  <td>{this.state.street}</td>
+                </tr>
+                <tr>
+                  <td>City</td>
+                  <td>{this.state.city}</td>
+                </tr>
+                <tr>
+                  <td>District</td>
+                  <td>{this.state.District}</td>
+                  </tr>
+                <tr>
+                  <td>Pincode</td>
+                  <td>{this.state.pincode}</td>
+                </tr>
+               </table>
+               </div>);  
+ }
+ }
+ App.defaultProps={
+     name:"Jhansi"
+ }
+ class TestMe extends Component{
+     render(){
+         return(<div><font face='Calibre' color='blue' size='20' >Hello All</font></div>);
+     }
+ }
+ class AddressSender extends Component{
+     render(){
+         return(<div><font color='red'>
+           {this.props.doornumber}
+           {this.props.street}
+           {this.props.city}
+           {this.props.District}
+           {this.props.pincode}</font></div>);
+     }
+ }
 export default App;
